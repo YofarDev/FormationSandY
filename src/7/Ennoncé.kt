933b23040d7ad05f531660cba439d77ddf9d3593
yofardev <yoann35@gmail.com>
2020-07-24 14:57:24 +0200

    commit

--- src/7/Ennoncé.kt
+++ src/7/Ennoncé.kt
@@ -2,12 +2,12 @@ package `7`
 
 fun main() {
 
-    val user1 = User("John Smith", "johnsmith@mail.com", 1988)
-    val user2 = User("Sarah Jackson", "hannah.03@hotmail", 2003)
-    val user3 = User("Clément Maton", "clecat-du-75@mail.com", 1991)
+    // val user1 = User("John Smith", "johnsmith@mail.com", 1988)
+    // val user2 = User("Sarah Jackson", "hannah.03@hotmail", 2003)
+    // val user3 = User("Clément Maton", "clecat-du-75@mail.com", 1991)
 
-    val users = listOf(user1, user2, user3)
-    for (user in users) user.printAge()
+    // val users = listOf(user1, user2, user3)
+    // for (user in users) user.printAge()
 
 }
 
